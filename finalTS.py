8e619f69d704a6f742ffb665ccb6c6ef55a3e428
rominavc99 <procrom1999@gmail.com>
2020-12-01 19:25:46 -0700

    codigo bien

--- finalTS.py
+++ finalTS.py
@@ -1,52 +1,162 @@
-import pyaudio
+import pyaudio 
 import numpy as np
 import wave
+from tkinter import *
 
+ventana = Tk()
+ventana.title("Analisis")
+ventana.configure(bg='beige')
 
-#Formato de audio de microfono
+ventana.geometry("350x300")
+
+#formato de audio de microfono
 PROFUNDIDAD_BITS = pyaudio.paInt16
 CANALES = 1
 FRECUENCIA_MUESTREO = 44100
-SEGUNDOS_GRABACION = 20
 
-#Tamaño del chunk
+#tamaño de chunk
 CHUNK = 2048
 
+SEGUNDOS_GRABACION = 1
+
+
+frecuenciaActual = StringVar()
+sonidoCuerda = StringVar()
+afinacion = StringVar()
+
+
+
+
+
 window = np.blackman(CHUNK)
+def iniciar():
+    
+    def analizar(stream):
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        #"2048h"
+        waveData = wave.struct.unpack("%dh"%(CHUNK), data)
+        npData=np.array(waveData)
 
-def analizar(stream):
-    data = stream.read(CHUNK, exception_on_overflow=False)
-    #"2048h"
-    waveData = wave.struct.unpack("%dh"%(CHUNK), data)
-    npData = np.array(waveData)
+        dataEntrada = npData * window
+        fftData = np.abs(np.fft.rfft(dataEntrada))
 
-    dataEntrada = npData * window
+        indiceFrecuenciaDominante = fftData[1:].argmax() + 1
+        
+        #cambio de indice  Hz
+        y0,y1,y2 = np.log(fftData[indiceFrecuenciaDominante-1: indiceFrecuenciaDominante+2])
+        x1 =(y2 - y0) * 0.5/(2 * y1 -y2 - y0)
+        frecuenciaDominante = (indiceFrecuenciaDominante + x1)* FRECUENCIA_MUESTREO/CHUNK
+        frecuencia = str(frecuenciaDominante)
+        print ("frecuencia dominante: "+ str(frecuenciaDominante) + "Hz", end='\r')
+        frecuenciaActual.set(frecuencia)
+        
+        tolerancia=13
+        toleranciaAfinacion = 1.3
 
-    fftData = np.abs(np.fft.rfft(dataEntrada))
+        if frecuenciaDominante > 82.4 - tolerancia and frecuenciaDominante < 82.4 + tolerancia:
+            Cuerda= "6ta Mi con una frecuencia de 82.4 Hz"
+            if frecuenciaDominante >82.4 - toleranciaAfinacion and frecuenciaDominante < 82.4 + toleranciaAfinacion:
+                Afinacion = "la afinacion es la correcta"
+            elif frecuenciaDominante < 82.4 + toleranciaAfinacion:
+                Afinacion = "debe apretar la cuerda "
+            else:
+                Afinacion = "debe aflojar la cuerda"
 
-    indiceFrecuenciaDominante = fftData[1:].argmax() + 1
+        elif frecuenciaDominante > 110.0 - tolerancia and frecuenciaDominante < 110.0 + tolerancia:
+            Cuerda= "5ta La con una frecuencia de 110.0 Hz"
+            if frecuenciaDominante >110.0 - toleranciaAfinacion and frecuenciaDominante < 110.0 + toleranciaAfinacion:
+                Afinacion = "la afinacion es correcta"
+            elif frecuenciaDominante < 110.0 + toleranciaAfinacion:
+                Afinacion = "debe apretar la cuerda "
+            else:
+                Afinacion = "debe aflojar la cuerda"
 
-    #Cambio de indice a Hz
-    y0,y1,y2 = np.log(fftData[indiceFrecuenciaDominante-1: indiceFrecuenciaDominante+2])
-    x1 = (y2 - y0) * 0.5 / (2 * y1 - y2 - y0)
-    frecuenciaDominante = (indiceFrecuenciaDominante+x1)*FRECUENCIA_MUESTREO/CHUNK
+        elif frecuenciaDominante > 146.83 - tolerancia and frecuenciaDominante < 146.83 + tolerancia:
+            Cuerda= "4ta Re con una frecuencia de 146.83 Hz"
+            if frecuenciaDominante >146.83 - toleranciaAfinacion and frecuenciaDominante < 146.83 + toleranciaAfinacion:
+                Afinacion = "la afinacion es la correcta"
+            elif frecuenciaDominante < 146.83 + toleranciaAfinacion:
+                Afinacion = "debe apretar la cuerda "
+            else:
+                Afinacion = "debe aflojar la cuerda"
 
-    print("Frecuencia dominante:" + str(frecuenciaDominante) + "Hz", end='\r')
+        elif frecuenciaDominante > 196.0 - tolerancia and frecuenciaDominante < 196.0 + tolerancia:
+            Cuerda= "3ra Sol con una frecuencia de 196.0 Hz"
+            if frecuenciaDominante >196.0 - toleranciaAfinacion and frecuenciaDominante < 196.0 + toleranciaAfinacion:
+                Afinacion = "la afinacion es la correcta"
+            elif frecuenciaDominante < 196.0 + toleranciaAfinacion:
+                Afinacion = "debe apretar la cuerda "
+            else:
+                Afinacion = "debe aflojar la cuerda"
+        
+        elif frecuenciaDominante > 246.94- tolerancia and frecuenciaDominante < 246.94 + tolerancia:
+            Cuerda= "2da Si con una frecuencia de 246.94 Hz"
+            if frecuenciaDominante >246.94 - toleranciaAfinacion and frecuenciaDominante < 246.94 + toleranciaAfinacion:
+                Afinacion = "la afinacion es la correcta"
+            elif frecuenciaDominante < 246.94 + toleranciaAfinacion:
+                Afinacion = "debe apretar la cuerda "
+            else:
+                Afinacion = "debe aflojar la cuerda"
 
-if __name__=="__main__":
-    p = pyaudio.PyAudio()
-    stream = p.open(format=PROFUNDIDAD_BITS, channels=CANALES, rate=FRECUENCIA_MUESTREO, input=True, frames_per_buffer=CHUNK)
+        elif frecuenciaDominante > 329.63 - tolerancia and frecuenciaDominante < 329.63  + tolerancia:
+            Cuerda= "1ra Mi con una frecuencia de  329.63 Hz"
+            if frecuenciaDominante >329.63  - toleranciaAfinacion and frecuenciaDominante < 329.63  + toleranciaAfinacion:
+                Afinacion = "la afinacion es la correcta"
+            elif frecuenciaDominante < 329.63  + toleranciaAfinacion:
+                Afinacion = "debe apretar la cuerda "
+            else:
+                Afinacion = "debe aflojar la cuerda"
+        else:
+            Cuerda = "la cuerda no ha identificada"
+            Afinacion = "presione de nuevo el boton de ingresar"
+        
 
-    for i in range(0, int(FRECUENCIA_MUESTREO * SEGUNDOS_GRABACION / CHUNK)):
-        analizar(stream)
+        sonidoCuerda.set(Cuerda)
+        
+        afinacion.set(Afinacion)
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
 
+        
 
 
     
+    if __name__=="__main__":
+   
+        p = pyaudio.PyAudio()
+        stream = p.open(format=PROFUNDIDAD_BITS, channels=CANALES, rate=FRECUENCIA_MUESTREO, input=True, frames_per_buffer=CHUNK)
+        
+        for i in range(0, int(FRECUENCIA_MUESTREO * SEGUNDOS_GRABACION / CHUNK)):
+            analizar(stream)
 
-         
 
+        
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+    
+
+boton = Button(ventana, text="Ingresar", command=iniciar, background="pink")
+boton.pack(pady=21)
+
+etiquetaCuerda = Label(ventana, textvariable=sonidoCuerda, background="beige")
+etiquetaCuerda.pack()
+
+etiqueta1=Label(ventana, textvariable=frecuenciaActual, background="beige")
+etiqueta1.pack()
+
+etiquetaAfinacion = Label(ventana, textvariable=afinacion, background="beige")
+etiquetaAfinacion.pack()
+
+
+ventana.mainloop()  
+   
+    
+
+
+
+
+
+boton = Button(ventana, text="Iniciar", command=iniciar)
+boton.pack()
+
+ventana.mainloop()
--- ventana.py
+++ ventana.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from finalTS import *
 
 
 
@@ -8,21 +9,22 @@ ventana.geometry("800x600")
 
 
 def apretar():
+    stream = StringVar()
     lblCuerda = Label(ventana, text="6ta Cuerda").pack()
     lblFrecuencia = Label(ventana, text="Frecuencia Actual").pack()
-    lblFrecuenciaDato = Label(ventana, text="80Hz").pack()
+    lblFrecuenciaDato = Label(ventana, textvariable=stream).pack()
     lblApretar = Label(ventana, text="Es necesario apretar").pack()
 
 def aflojar():
     lblCuerda = Label(ventana, text="6ta Cuerda").pack()
     lblFrecuencia = Label(ventana, text="Frecuencia Actual").pack()
-    lblFrecuenciaDato = Label(ventana, text="80Hz").pack()
+    lblFrecuenciaDato = Label(ventana, textvariable=stream).pack()
     lblAflojar = Label(ventana, text="Es necesario aflojar").pack()
 
 def correcto():
     lblCuerda = Label(ventana, text="6ta Cuerda").pack()
     lblFrecuencia = Label(ventana, text="Frecuencia Actual").pack()
-    lblFrecuenciaDato = Label(ventana, text="80Hz").pack()
+    lblFrecuenciaDato = Label(ventana, textvariable=stream).pack()
     lblCorreto = Label(ventana, text="La frecuencia es correcta").pack()
 
 def iniciar():
